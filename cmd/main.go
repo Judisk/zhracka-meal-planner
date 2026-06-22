63860a33fd4c082d6431aba385b60c57edd1b7f3
Judisk <JudiskUA@gmail.com>
2026-06-22 11:42:29 +0300

    feat: add storage package with NewDB and products table

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	d "foods/internal/dayone"
 	f "foods/internal/foodgenerator"
 	p "foods/internal/products"
+	"foods/internal/storage"
 )
 
 func main() {
 	fmt.Println(f.GenerateDish("Test name #1", p.Grains, p.Proteins, p.Vegetables, nil))
 
 	fmt.Println(d.GenerateMeals(3, p.Grains, p.Proteins, p.Vegetables, nil))
+
+	db, err := storage.NewDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 }
---
+++ internal/storage/storage.go
@@ -0,0 +1,33 @@
+package storage
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "modernc.org/sqlite"
+)
+
+func NewDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite", "products.db")
+	if err != nil {
+		return nil, fmt.Errorf("sql open: %w", err)
+	}
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("sql ping: %w", err)
+	}
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS products (
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    name     TEXT NOT NULL,
+    category TEXT NOT NULL,
+    banned   BOOLEAN DEFAULT FALSE,
+    favorite BOOLEAN DEFAULT FALSE
+);
+	`)
+	if err != nil {
+		db.Close()
+		return nil, fmt.Errorf("sql get result:%w", err)
+	}
+	return db, nil
+}
